a444876786c71dbf9237938c265ad879afacdc56
Ildar <106691203+Ranhorn123@users.noreply.github.com>
2022-07-15 16:37:41 +0400

    feat:test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function reverse (n) {
-  
+    
+   
+    return  String(Math.abs(n)).split("").reverse().join("");
 }
+
+
+/*module.exports =function reverse (n) {
+    let r = "";
+    for (const i = n.length - 1; i >= 0; i-1) {
+        r += n[i];
+    }
+    return r
+}
+*/
+/*module.exports = function reverse (n)
+{
+    n+="";
+    const arr = [...n]
+    let r ="";
+  
+    while(arr.length){
+       r = r + arr.pop()
+    }
+ 
+    return +r;
+}
+
+*/
